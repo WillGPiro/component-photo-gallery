568e5d7c231d7dc6869358d93620efc41734f682
Will G Piro <willgpiro@gmail.com>
2020-02-12 12:18:03 -0800

    added filter and map function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HornedList from './HornedList';
 import Header from './Header';
-import HornedItem from './HornedItem';
-import images from './data';
-
 
 function App() {
   return (
